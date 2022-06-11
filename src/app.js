1133de96cd9151003af60584ded45667f703946f
Roberto Lopez J <roberto@lightsaint.com>
2022-06-11 02:30:13 +0000

    [Added a random Card generator]
    - Added html & css of a poker card and button
    - added JS with functionality to generate a random card every time
    it's clicked

--- src/app.js
+++ src/app.js
@@ -7,5 +7,38 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let symUp = document.querySelector(".card-symbol-up");
+  let symDow = document.querySelector(".card-symbol-down");
+
+  function randomCard() {
+    let rand = Math.floor(Math.random() * 14) + 2;
+    let randLetter = ["J", "Q", "K", "A"];
+    var letter = randLetter[Math.floor(Math.random() * randLetter.length)];
+
+    if (rand > 10) {
+      return letter;
+    } else return rand;
+  }
+
+  function randomSymbol() {
+    let symbols = ["♦", "♥", "♠", "♣"];
+    let symbol = symbols[Math.floor(Math.random() * symbols.length)];
+
+    if (symbol == "♥") {
+      symUp.style.color = "red";
+      symDow.style.color = "red";
+    } else if (symbol == "♦") {
+      symUp.style.color = "red";
+      symDow.style.color = "red";
+    }
+    return symbol;
+  }
+
+  let letter = document.querySelector("#midLetter");
+  letter.innerHTML = randomCard();
+  symUp.innerHTML = symDow.innerHTML = randomSymbol();
 };
+
+let button = document.querySelector(".btn");
+button.addEventListener("click", window.onload);
